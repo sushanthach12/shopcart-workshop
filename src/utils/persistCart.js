c2c6b98a2d4b0d2d08cd04a115cdda15c740c180
Sushanth Acharya <84125038+sushanthach12@users.noreply.github.com>
2023-10-27 16:53:25 +0530

    Update persistCart.js

--- src/utils/persistCart.js
+++ src/utils/persistCart.js
@@ -2,6 +2,9 @@
 export const saveCart = async (item) => {
     let localStore = localStorage.getItem('cart')
     console.log(JSON.parse(localStore))
+
+    // TODO : check if item already pressent in cart if not add to cart, if yes show item already present in cart
+    
     if(localStore !== null) {
         localStorage.setItem('cart', JSON.stringify([...JSON.parse(localStore), item]));
     }else {
