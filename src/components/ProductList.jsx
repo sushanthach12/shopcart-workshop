711411adab64db491fa3cefc8ad5239a6d7d7739
Sushanth Acharya <84125038+sushanthach12@users.noreply.github.com>
2023-10-30 11:53:45 +0530

    Update ProductList.jsx

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -41,33 +41,6 @@ const ProductList = () => {
 						</div>
 					))
 				}
-
-				<div class="card mb-3 w-75" >
-					<div class="card-body">
-						<div class="d-flex justify-content-between">
-							<div class="d-flex flex-row align-items-center">
-								<div>
-									<img
-										src={"..."}
-										class="img-fluid rounded-3" alt="Shopping item" style={{ width: '65px' }} />
-								</div>
-								<div class="ms-3">
-									<h5>sdvsvd</h5>
-									<p class="small mb-0">sdvsdv</p>
-								</div>
-							</div>
-							<div class="d-flex flex-row align-items-center">
-								<div style={{ width: '50px' }}>
-									<button type="button" class="btn btn-danger" onClick={() => updatePro(123)}>del</button>
-								</div>
-								<div style={{ width: '85px' }}>
-									<h5 class="mb-0">₹344</h5>
-								</div>
-								<a href="#!" style={{ color: '#cecece' }}><i class="fas fa-trash-alt"></i></a>
-							</div>
-						</div>
-					</div>
-				</div>
 			</div>
 		</div>
 
